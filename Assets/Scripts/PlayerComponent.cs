677cf8f96c6feaa7f3ca127923533bf153fd02a8
KhaZixNb1Fan <n_niruhtam@hotmail.ca>
2025-09-23 13:16:57 -0400

    Movement +++

--- Assets/Scripts/PlayerComponent.cs
+++ Assets/Scripts/PlayerComponent.cs
@@ -1,25 +1,44 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerComponent : MonoBehaviour
 {
+
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float gravityValue = -9.81f;
     [SerializeField] private float fallMultiplier = 2.5f;
-
+    [SerializeField] private int maxJumpCount = 2;
     private bool fastFall = false;
     float multiplier;
 
+    // Jumping
+    int jumpCount = 0;
+
+    //Dashing
+    private bool canDash = true;
+    private bool isDashing = false;
+    [SerializeField] float dashPower = 20f;
+    private float dashTime = 0.2f;
+    private float dashCooldown = 1f;
+
+    // Movement
     Vector3 direction = Vector3.zero;
     Vector3 jump = Vector3.zero;
     Vector2 move = Vector2.zero;
 
+    // Components
+    PlayerInput playerInput;
     CharacterController characterController;
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.actions["Jump"].started += Jump;
+        playerInput.actions["Jump"].canceled += Jump;
     }
 
     // Update is called once per frame
@@ -34,9 +53,9 @@ public class PlayerComponent : MonoBehaviour
         direction = new Vector3(move.x, 0, 0).normalized;
 
         multiplier = fastFall ? fallMultiplier : 1f;
-        if (characterController.isGrounded)
+        if (characterController.isGrounded && jump.y <= 0)
         {
-            jump.y = Mathf.Max(-1, jump.y);
+            jumpCount = 0;
         }
         else
         {
@@ -53,15 +72,50 @@ public class PlayerComponent : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed && characterController.isGrounded)
+        if (ctx.performed && jumpCount < maxJumpCount)
         {
             jump = transform.up * jumpForce;
+            jumpCount++;
         }
+    }
+    public void Dash(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed && canDash && !isDashing)
+            StartCoroutine(DashCoroutine());
+    }
+    public IEnumerator DashCoroutine()
+    {
+        canDash = false;
+        isDashing = true;
+
+        float gravity = gravityValue;
+        gravityValue = 0;
+
+        float elapsed = 0f;
+        Vector3 dashDir = new Vector3(move.x, 0, 0).normalized;
+
+        while (elapsed < dashTime)
+        {
+            // Smoothly decrease dash speed over time
+            //act as a progress bar
+            float t = elapsed / dashTime;
+            //act as a decrease of speed over time based on t
+            float speedDash = Mathf.Lerp(dashPower, 0f, t);
+
+            characterController.Move(dashDir * speedDash * Time.deltaTime);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        isDashing = false;
+        gravityValue = gravity;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 
     public void FastFall(InputAction.CallbackContext ctx)
     {
         fastFall = ctx.performed;
     }
-
 }
