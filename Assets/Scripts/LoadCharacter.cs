9745f7ce221843b7db2f0fc0d203398237034056
marc_ <marc_antoine_faucher@hotmail.com>
2025-09-24 12:38:54 -0400

    Selection bouton
    Ajout selection de personnage avec bouton

---
+++ Assets/Scripts/LoadCharacter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TMPro;
+
+
+public class LoadCharacter : MonoBehaviour
+{
+    public GameObject[] characterPrefabs;
+    public Transform spawnPoint;
+    public TMP_Text label;
+    public void Start()
+    {
+        int selectedCharacter = PlayerPrefs.GetInt("SelectedCharacter");
+        GameObject prefab = characterPrefabs[selectedCharacter];
+        label.text = prefab.name;
+    }
+}
