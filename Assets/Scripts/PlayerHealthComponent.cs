c7d0d35b0e9cc381b3a9d3f93b3137cff4a0c122
KhaZixNb1Fan <n_niruhtam@hotmail.ca>
2025-09-24 09:33:38 -0400

    BaseHealth and Rename of Movement Script

---
+++ Assets/Scripts/PlayerHealthComponent.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerHealthComponent : MonoBehaviour
+{
+
+    [SerializeField] float maxHealth = 100f;
+    [SerializeField] float chipSpeed = 2f;
+
+    [SerializeField] Image frontHealthBar;
+    [SerializeField] Image backHealthBar;
+
+    public bool isDead;
+    private float health;
+    private float elapsedSinceHit;
+    private Coroutine backBarCoroutine;
+
+    void Start()
+    {
+        health = maxHealth;
+
+        frontHealthBar.fillAmount = 1f;
+        backHealthBar.fillAmount = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            TakeDamage(10f);
+        }
+
+    }
+    public void TakeDamage(float damage)
+    {
+        if (isDead) return;
+        health -= damage;
+        health = Mathf.Clamp(health, 0f, maxHealth);
+        elapsedSinceHit = 0f;
+
+        if (health <= 0)
+        {
+            isDead = true;
+
+        }
+        Debug.Log($"Player Health: {health}");
+        UpdateHealthUI();
+    }
+
+    private void UpdateHealthUI()
+    {
+        float targetFill = health / maxHealth;
+
+        frontHealthBar.fillAmount = targetFill;
+
+        if (backBarCoroutine != null)
+            StopCoroutine(backBarCoroutine);
+        backBarCoroutine = StartCoroutine(AnimateBackBar(backHealthBar.fillAmount, targetFill));
+    }
+
+    private IEnumerator AnimateBackBar(float fromFill, float toFill)
+    {
+        float elapsed = 0f;
+        while (elapsed < chipSpeed)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / chipSpeed);
+            backHealthBar.fillAmount = Mathf.Lerp(fromFill, toFill, t);
+            yield return null;
+        }
+        backHealthBar.fillAmount = toFill;
+    }
+}
--- Assets/Scripts/PlayerMovementComponent.cs
+++ Assets/Scripts/PlayerMovementComponent.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerComponent : MonoBehaviour
+public class PlayerMovementComponent : MonoBehaviour
 {
     //Movement
     [SerializeField] private float speed = 5f;
