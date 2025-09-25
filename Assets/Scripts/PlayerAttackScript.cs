a4c66eb6163b4c2acd8c613a994d5672a35dcfe5
KhaZixNb1Fan <n_niruhtam@hotmail.ca>
2025-09-24 23:19:16 -0400

    Ui + Attack start

--- Assets/Scripts/PlayerAttackScript.cs
+++ Assets/Scripts/PlayerAttackScript.cs
@@ -1,16 +1,80 @@
 using UnityEngine;
 
+public enum ClassType
+{
+    Warrior,
+    Mage,
+    Assassin,
+}
+
+public class CharacterStats
+{
+    public float health;
+    public float damage;
+    public float attackSpeed;
+    public float speed;
+}
 public class PlayerAttackScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] ClassType classType;
+    private PlayerHealthComponent player;
+    private CharacterStats characterStats;
     void Start()
     {
-        
+
+        player = GetComponent<PlayerHealthComponent>();
+        characterStats = GetStatsForClass(classType);
     }
 
-    // Update is called once per frame
+    public CharacterStats GetStatsForClass(ClassType type)
+    {
+        switch (type)
+        {
+            case ClassType.Warrior:
+                return new CharacterStats
+                {
+                    health = 150f,
+                    damage = 7f,
+                    attackSpeed = 1f,
+                    speed = 5f
+                };
+            case ClassType.Mage:
+                return new CharacterStats
+                {
+                    health = 100f,
+                    damage = 13f,
+                    attackSpeed = 0.7f,
+                    speed = 4f
+                };
+            case ClassType.Assassin:
+                return new CharacterStats
+                {
+                    health = 100f,
+                    damage = 11f,
+                    attackSpeed = 1.5f,
+                    speed = 6f
+                };
+            default:
+                return null;
+        }
+    }
+
     void Update()
     {
-        
+
+    }
+
+    public void Attack()
+    {
+
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player") && )
+        {
+            player.TakeDamage(damage);
+        }
     }
 }
